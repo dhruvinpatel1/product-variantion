7f18a1266362bd07a2afd523142d0c1814dc95df
dhruvinpatel1 <dhruvin@netfillip.com>
2025-08-07 16:11:31 +0530

    changes

--- app/routes/webhooks.app.uninstalled.jsx
+++ app/routes/webhooks.app.uninstalled.jsx
@@ -4,8 +4,7 @@ import db from "../db.server";
 export const action = async ({ request }) => {
   const { shop, session, topic } = await authenticate.webhook(request);
 
-  console.log(`uninstall app Received ${topic} webhook for ${shop}`);
-  console.log(`session Topic: ${JSON.stringify(session)}`);
+  console.log(`Received ${topic} webhook for ${shop}`);
 
   // Webhook requests can trigger multiple times and after an app has already been uninstalled.
   // If this webhook already ran, the session may have been deleted previously.
